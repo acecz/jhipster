8bb54b82d7735aac5aeb7c5ccd85399f29d4f8c0
alwyntso <alwyntso@gmail.com>
2018-04-22 15:38:47 +0800

    update

---
+++ angular/start/my-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './core/auth.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [
+    { provide: 'auth', useClass: AuthService }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
